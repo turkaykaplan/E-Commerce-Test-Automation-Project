90c57dac1d28d30e4f9e707ece98ae29238b0b43
Türkay Kaplan <82119064+turkaykaplan@users.noreply.github.com>
2023-03-01 16:29:43 +0300

    Update HomePage.java

--- ECommerceTest/src/main/java/HomePage.java
+++ ECommerceTest/src/main/java/HomePage.java
@@ -18,13 +18,13 @@ public class HomePage extends BasePage {
 
     public void search(){
         driver.findElement(By.id("navbar-search-input")).sendKeys("apple");
-        driver.findElement(By.xpath("//span[@class='icon-search']")).click(); // /html/body/header/nav/div[3]/div[1]/div/div/div[2]/div[2]/div/div/button
+        driver.findElement(By.xpath("//span[@class='icon-search']")).click(); 
     }
 
     public void selectItemMenu() throws InterruptedException {
         driver.findElement(By.xpath("/html/body/div[8]/div/a")).click();
         //Thread.sleep(500);
-        driver.findElement(By.xpath("//*//a[text()='iPhone']")).click(); // /html/body/main/div/div/div/div[3]/div[1]/div[1]/a
+        driver.findElement(By.xpath("//*//a[text()='iPhone']")).click(); 
 }
 
 
